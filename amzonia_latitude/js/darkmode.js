44993ef7255c901e49c2de09be33170cc60baea7
Ana <anacaroline2744@outlook.com>
2025-01-14 11:52:01 -0300

    ver. 14.01.25
    Pré-formato do nav-bar list, formatação de título, banner. Nav-bar foi refeito para os links corretos e folha de estilo.

--- amzonia_latitude/js/darkmode.js
+++ amzonia_latitude/js/darkmode.js
@@ -1,11 +1,12 @@
 const colorMode = document.getElementById("colormode");
 const body = document.getElementById("body");
-
+const dfooter = document.getElementById("footer_content")
 colorMode.addEventListener("change", () => {
     // if(body.classname != "dark-mode"){
     //     body.className = "dark-mode";
     // }else {
     //     body.className = "white-mode";    
     // }       
-    document.body.classList.toggle("dark-mode")
+    document.body.classList.toggle("dark-mode");
+    document.dfooter.classList.toggle("dark-mode");
 });
