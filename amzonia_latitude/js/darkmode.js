b8933a39392453a18065602c8dd19753ff84e936
Ana <anacaroline2744@outlook.com>
2025-01-21 10:07:40 -0300

    Código revisado, ainda sem o dark-mode aplicado ao sobre.

--- amzonia_latitude/js/darkmode.js
+++ amzonia_latitude/js/darkmode.js
@@ -1,12 +1,12 @@
 const colorMode = document.getElementById("colormode");
 const body = document.getElementById("body");
 const dfooter = document.getElementById("footer_content");
+const main = document.getElementById("main");
 
 colorMode.addEventListener("change", () => {
        
     document.body.classList.toggle("dark-mode");
+    document.main.classList.toggle("dark-mode");  
     document.dfooter.classList.toggle("dark-mode");
-    document.main.classList.toggle("dark-mode");    
-    document.dk.classList.toggle("dark-mode");
     
 });
