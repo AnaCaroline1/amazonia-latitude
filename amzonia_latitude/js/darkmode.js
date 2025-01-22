5e11d4c989f9923cc805b2c39e35203c18f7bc6f
Ana <anacaroline2744@outlook.com>
2025-01-21 23:05:51 -0300

    ver. 21.01.25
    Esta é a versão parcial do site, como o dark-mode funcionando.

--- amzonia_latitude/js/darkmode.js
+++ amzonia_latitude/js/darkmode.js
@@ -1,11 +1,12 @@
 const colorMode = document.getElementById("colormode");
 const body = document.getElementById("body");
-
+const main = document.getElementById("main");
 colorMode.addEventListener("change", () => {
     // if(body.classname != "dark-mode"){
     //     body.className = "dark-mode";
     // }else {
     //     body.className = "white-mode";    
     // }       
-    document.body.classList.toggle("dark-mode")
+    document.body.classList.toggle("dark-mode");
+    document.main.classList.toggle("dark-mode");
 });
